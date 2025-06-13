2d4a42b49919478b6cb0b49830ac2e55f11a4109
Victor <Victor@ULTRA-7-265KF>
2025-06-13 23:27:25 +0300

    67

--- src/theme.tsx
+++ src/theme.tsx
@@ -1,6 +1,5 @@
 import { createTheme } from '@mui/material/styles';
 
-
 const theme = createTheme({
   palette: {
     primary: {
@@ -11,42 +10,10 @@ const theme = createTheme({
     MuiButton: {
       styleOverrides: {
         root: {
-          // color: '#fff', 
           color: '#303A47',
         },
-        // contained: {
-        //   backgroundColor: '#303A47',
-        //   '&:hover': {
-        //     backgroundColor: '#303A47',
-        //   },
-        // },
       },
     },
   },
 });
 export default theme;
-
-
-// import { createTheme } from '@material-ui/core/styles';
-// const theme = createTheme({
-//   palette: {
-//     primary: {
-//       main: '#303A47', 
-//     },
-//   },
-//   overrides: {
-//     MuiButton: {
-//       root: {
-//         color: '#303A47', 
-//       },
-//       contained: {
-//         backgroundColor: '#303A47', 
-//         '&:hover': {
-//           backgroundColor: '#303A47', 
-//         },
-//       },
-//     },
-//   },
-// });
-
-// export default theme;
